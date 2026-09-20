02d81d87aefe38623c913238df78c7629c01c29d
Linguine <joaovictor.matosalmeida@gmail.com>
2026-09-20 14:03:25 -0300

    Alterado concatenação de variaveis dentro do alert e feito a junção das variáveis com seus respectivos input()

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,25 @@
 nome = prompt("Qual é o seu nome?");
-alert("Olá, " + nome + "! Seja bem-vindo à Vinharia Agnello!");
-console.log("Nome do usuário: " + nome);
+alert(`Olá,  ${nome}! Seja bem-vindo à Vinharia Agnello!`);
+console.log(`Nome do usuário: ${nome}`);
 alert('Cadastro realizado! Veja os detalhes no console');
 
 alert("por favor tenha as informações do vinho que deseja cadastrar em mãos, para que o cadastro seja realizado com sucesso!");
 
 nomeVinho = prompt("Qual é o nome do vinho que você deseja cadastrar?"); 
 
-console.log("Nome do vinho: " + nomeVinho);
+console.log(`Nome do vinho: ${nomeVinho}`);
 
 tipoVinho = prompt("Qual é o tipo do vinho que você deseja cadastrar? (Tinto, Branco, Rosé)");
 
-console.log("Tipo do vinho: " + tipoVinho);
+console.log(`Tipo do vinho: ${tipoVinho}`);
 
 safraVinho = prompt("Qual é a safra do vinho que você deseja cadastrar?");
 
-console.log("Safra do vinho: " + safraVinho);
+console.log(`Safra do vinho: ${safraVinho}`);
 
 quantidadeVinho = prompt("Qual é a quantidade do vinho que você deseja cadastrar?");
 
-console.log("Quantidade do vinho: " + quantidadeVinho);
+console.log(`Quantidade do vinho: ${quantidadeVinho}`);
 
 alert("Cadastro do vinho realizado! Veja os detalhes no console");
 
